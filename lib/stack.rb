369715ba9e8abb029b54875e6d6c46f6987f7204
David Tewkesbury <tewkesburyd@hotmail.com>
2023-01-16 15:15:49 -0700

    commit tree 70b77698e4aae731bf0e75bd8a3c93475115dfe1

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,42 @@
 # your code here
+class Stack
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(el)
+        raise 'Stack Overflow' if full?
+        @stack.push(el)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @limit && @stack.size == @limit
+    end
+
+    def search(target)
+        @stack.each_with_index do |item, idx|
+            return size - idx - 1 if item == target
+          end
+      
+          -1
+    end
+
+end
